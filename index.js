9694dfb9b876a6a7960e8ce7b2b247893f1290bb
HelloWorld7894 <danpojhan@gmail.com>
2022-09-20 19:56:33 +0200

    Added some content

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function Description(index){
     var Description = document.getElementsByClassName("Header")[index]
 
     Description.style.filter = "none"
+    Mask.firstElementChild.style.visibility = "hidden"
     Mask.classList.add("shiftLeft")
     Mask.style.visibility = "hidden"
 }
