84ac19482b0c1de74fac8669f29c2ec7ba836b24
HelloWorld7894 <danpojhan@gmail.com>
2022-09-21 22:29:12 +0200

    made some significant changes

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function Description(index){
-    var Mask = document.getElementsByClassName("HeaderMask")[index]
-    var Description = document.getElementsByClassName("Header")[index]
-
-    Description.style.filter = "none"
-    Mask.firstElementChild.style.visibility = "hidden"
-    Mask.classList.add("shiftLeft")
-    Mask.style.visibility = "hidden"
-}
-
 document.onload = () => {
+    /*Empty*/
     console.log("Ready")
 }
